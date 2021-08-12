d29b6aa0a3a47f0bed2e39779caacdb681269734
Gregory Macat <gregorymacat@gmail.com>
2021-08-12 16:56:12 -0400

    Added routes on DB side. First route functioning

--- Server/router.js
+++ Server/router.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const {getAll} = require('../Database/index.js');
+const fetchers = require('../Database/index.js');
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
 router.get('/test', (req, res) => {
-  getAll()
+
+});
+router.get('/products', (req, res) => {
+  fetchers.getMultipleProducts(req.query.page, req.query.count)
     .then(response => {
       console.log('Success retrieving data');
       return res.send(response.rows);
     })
     .catch(err => {
-      return console.error('UNABLE TO RETRIEVE DATA FROM DATABASE ', err)
-    })
-  ;
-});
-router.get('/products', (req, res) => {
-  res.send(req.query.page);
+      console.error('UNABLE TO RETRIEVE DATA FROM DATABASE ', err);
+      return res.sendStatus(404);
+    });
 });
 router.get('/products/:product_id', (req, res) => {
   res.send(req.params);

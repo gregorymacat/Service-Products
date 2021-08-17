403908f6fdabadb05334a4c367b39512389841bb
Gregory Macat <gregorymacat@gmail.com>
2021-08-17 17:33:55 -0400

    Updating test for variability

--- Tests/smoke.js
+++ Tests/smoke.js
@@ -6,14 +6,19 @@ export var options = {
   duration: '1m', // for 1 minute
 
   thresholds: {
-    http_req_duration: ['p(99)<2000'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<2000'], // 99% of requests must complete below 2s
   },
 }
-
-const URL = 'http://localhost:3000/products';
+const URLs = {
+  allProducts: 'http://localhost:3000/products',
+  allProductsSpecified: 'http://localhost:3000/products?page=5&count=20',
+  oneProduct: 'http://localhost:3000/products/1',
+  styles: 'http://localhost:3000/products/1/styles',
+  related: 'http://localhost:3000/products/1/related'
+}
 
 export default function () {
-  var manyProductRes = http.get(URL);
-  check(manyProductRes, {'retrieved prodcuts successfully': (r) => r.status == 200 });
+  var productRes = http.get(URLs.oneProduct);
+  check(productRes, {'retrieved prodcuts successfully': (r) => r.status == 200 });
   sleep(1);
 }
